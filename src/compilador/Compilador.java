f7d57d049dcf62567af17f636c8986cea35e0af1
lin99 <lin.rosales@gmail.com>
2014-10-31 18:28:42 -0500

    Prueba Compilador
    Commit de prueba

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -1,5 +1,17 @@
 package compilador;
 
-public class Compilador {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
 
+public class Compilador {
+	
+	//Cambiar path del archivo de escritura
+	public void crearTest( String nameFile ) throws Exception{
+		BufferedReader br = new BufferedReader( new FileReader( nameFile ) );
+		PrintWriter pw = new PrintWriter("Test.java");
+	}
+	
+	
 }
